4bed1d61300efc3c2388ada7cfcf3817a5a6bafd
樵夫 <43578612+Justinshu@users.noreply.github.com>
2019-03-21 11:39:15 +0800

    Create main.py
    使用自定义版本

---
+++ main.py
@@ -0,0 +1,9 @@
+def publisher_list(request):
+    # 从URL中取当前访问的页码数
+    current_page = int(request.GET.get('page'))
+    # 比len(models.Publisher.objects.all())更高效
+    total_count = models.Publisher.objects.count()
+    page_obj = Pagination(current_page, total_count, request.path_info)
+    data = models.Publisher.objects.all()[page_obj.start:page_obj.end]
+    page_html = page_obj.page_html()
+    return render(request, "publisher_list.html", {"publisher_list": data, "page_html": page_html})
